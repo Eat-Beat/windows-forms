2fc1bae63377a25caff1af99fe65cb3b455a55a6
jordibonet <jboneta2122@politecnics.barcelona>
2025-03-06 18:50:27 +0100

    add mapa

--- Eat&Beat/Forms/FormMusicianUsers.cs
+++ Eat&Beat/Forms/FormMusicianUsers.cs
@@ -26,7 +26,7 @@ namespace Eat_Beat.Forms
 
         private void roundedButtonCreate_Click(object sender, EventArgs e)
         {
-            formLogin.LoadFormIntoPanel("FormNewMusician2", true);
+            
         }
     }
 }
